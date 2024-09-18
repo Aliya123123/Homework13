6f437347b925647d859cd68a996286b2b6f42e7b
user <transakon77@gmail.com>
2024-09-18 22:29:26 +0300

    3.0

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,19 @@ class Author {
     public String toString() {
         return "Имя " + this.name + ", Фамилия " + this.surname;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 }
 
 
@@ -66,7 +79,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("task 1");
         Author author1 = new Author("Кристиан ", "Ханс");
-        Author author2 = new Author("Шарль", " Перро");
+        Author author2 = new Author("Шарль ", "Перро");
         Book one = new Book ("Русалочка",author1, 2013);
         Book two  = new Book ("Золушка",author2,2020 );
         System.out.println(one);
@@ -74,7 +87,7 @@ public class Main {
         one.setYearBook(2016);
         System.out.println("YearBook = " + one.getYearBook());
         System.out.println(one.equals(two));
+        System.out.println(author1.equals(author2));
         System.out.println(Objects.hash());
     }}
-
 
